7c3dcc1ead8bb209e7c8ec727763b63271bf7406
santiagoAlvRu <santiagoesluz@gmail.com>
2025-06-25 17:47:11 +0200

    last details

--- functions.php
+++ functions.php
@@ -12,8 +12,7 @@ function cleanInputs($input)
 
 function fileUpload($picture, $source = "user")
 {
-    if ($picture["error"] == 4) { // checking if a file has been selected, it will return 0 if you choose a file, and 4 if you didn't
-        $pictureName = "avatar.png";
+    if ($picture["error"] == 4) {
 
         if ($source == "pet") {
             $pictureName = "pet.png";
@@ -21,19 +20,19 @@ function fileUpload($picture, $source = "user")
 
         $message = "No picture has been chosen, but you can upload an image later :)";
     } else {
-        $checkIfImage = getimagesize($picture["tmp_name"]); // checking if you selected an image, return false if you didn't select an image
+        $checkIfImage = getimagesize($picture["tmp_name"]);
         $message = $checkIfImage ? "Ok" : "Not an image";
     }
 
     if ($message == "Ok") {
-        $ext = strtolower(pathinfo($picture["name"], PATHINFO_EXTENSION)); // taking the extension data from the image
-        $pictureName = uniqid("") . "." . $ext; // changing the name of the picture to random string and numbers
-        $destination = "img/{$pictureName}"; // where the file will be saved
+        $ext = strtolower(pathinfo($picture["name"], PATHINFO_EXTENSION));
+        $pictureName = uniqid("") . "." . $ext;
+        $destination = "img/{$pictureName}";
         if ($source == "pet") {
-            $destination = "../img/{$pictureName}"; // where the file will be saved
+            $destination = "../img/{$pictureName}";
         }
-        move_uploaded_file($picture["tmp_name"], $destination); // moving the file to the pictures folder
+        move_uploaded_file($picture["tmp_name"], $destination);
     }
 
-    return [$pictureName, $message]; // returning the name of the picture and the message
+    return [$pictureName, $message];
 }
